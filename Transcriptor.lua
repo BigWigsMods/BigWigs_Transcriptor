238acca22f0cb431cbbe10cfe654dd979e4a7b06
Kyle Buller <bullerk@gmail.com>
2015-01-25 05:02:27 +0000

    cancel if the pull timer is stopped

--- Transcriptor.lua
+++ Transcriptor.lua
@@ -218,7 +218,7 @@ function plugin:BigWigs_StartPull(_, _, seconds)
 	end
 end
 
-function plugin:BigWigs_StopBar(_, text)
+function plugin:BigWigs_StopBar(_, _, text)
 	if text == PL.pull and timer then
 		self:CancelTimer(timer)
 		timer = nil
